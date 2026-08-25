e437698d8698ad60f0c9cb611f029470f54268c4
MPelland-programming <maxime.pelland.programming@gmail.com>
2026-08-25 16:33:58 -0400

    Updated gathering of results

--- main_statsprep.py
+++ main_statsprep.py
@@ -1,6 +1,14 @@
+#First set up the environment.
+
+import generalfunctions as gf
+from pathlib import Path
+import os
 import statprep as sp
 
-model_file = "/home/mpelland/links/projects/def-eporte2/mpelland/predictability/Configs/NoContext1/model.csv"
+sub_folder = Path(os.getcwd())
+main_file = Path(sub_folder.parent,"model.csv")
+out_file = Path(sub_folder,"model.csv")
 
+gf.multi_join(main_file,sub_folder)
 
-sp.gmm_setup()
+sp.gmm_setup(out_file,time_agg="wmean",parent_agg="wmean",min_age_gap=3)
